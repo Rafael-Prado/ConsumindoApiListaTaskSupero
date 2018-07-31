79bfa77e2620375129a6eee948182fe1e51f1655
Rafael-Prado <rafaeldacruzdoprado@hotmail.com>
2018-07-31 17:24:58 -0300

    correção nas rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,7 +9,11 @@ const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'tasks', component: TasksComponent },
   { path: 'tasksResolved', component: TasksResolvedComponent },
-  { path: 'tasksWaitingo', component: TasksWaitingoComponent }
+  { path: 'tasksWaitingo', component: TasksWaitingoComponent },
+  { path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
   ];
 
 @NgModule({
